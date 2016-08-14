3292ce631e863c14a13333dcd06682cb62926e96
rhykw <dev@rhykw.net>
2016-08-15 08:44:07 +0900

    modify build-scripts/travis-install-pdns.sh

--- build-scripts/travis-install-pdns.sh
+++ build-scripts/travis-install-pdns.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -ex
 cat <<'__EOF__'>/etc/apt/sources.list.d/pdns.list
 deb [arch=amd64] http://repo.powerdns.com/ubuntu trusty-auth-40 main
 __EOF__
@@ -10,5 +11,6 @@ Pin: origin repo.powerdns.com
 Pin-Priority: 600
 __EOF__
 
-wget -O - https://repo.powerdns.com/FD380FBB-pub.asc | apt-key add - &&
-apt-get install pdns-server
+wget -O - https://repo.powerdns.com/FD380FBB-pub.asc | apt-key add -
+apt-get -y -qq update
+apt-get -y -qq install pdns-server
