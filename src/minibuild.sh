e77e33e3bc04212ef9497389758f6c3111dfb3cb
rhykw <dev@rhykw.net>
2016-08-15 02:56:19 +0900

    modify minibuild.sh

--- src/minibuild.sh
+++ src/minibuild.sh
@@ -12,10 +12,10 @@ PDNS_INSTALL_DIR=/usr/local/pdns
 
 ( rm *.so *.o || true )
 
-gcc -g -fPIC -I. -I${MRUBY_INCLUDE_DIR} -c pdns_mruby_core.cc
-g++ -g -fPIC -I. -I${MRUBY_INCLUDE_DIR} -I${PDNS_BUILD_DIR} -I${PDNS_SRC_DIR} -c mrubybackend.cc
+cc -g -fPIC -I. -I${MRUBY_INCLUDE_DIR} -c pdns_mruby_core.cc
 
-g++ -g -shared -fPIC -o libmrubybackend.so pdns_mruby_core.o mrubybackend.o ${LIBMRUBY} $LIBS
+c++ -Wall -std=gnu++11 -g -fPIC -I. -I${MRUBY_INCLUDE_DIR} -I${PDNS_BUILD_DIR} -I${PDNS_SRC_DIR} -c mrubybackend.cc
+c++ -Wall -std=gnu++11 -g -shared -fPIC -o libmrubybackend.so pdns_mruby_core.o mrubybackend.o ${LIBMRUBY} $LIBS
 
 # sudo cp libmrubybackend.so ${PDNS_INSTALL_DIR}/lib/pdns/
 # cp ../pdns-mruby.conf.sample ${PDNS_INSTALL_DIR}/etc/pdns.conf
